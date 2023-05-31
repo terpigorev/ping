0354d2e5d7df545e0e68f6eadad8a37cf1ce57d2
Михаил Терпигорев <mihailterpigorev@MacBook-Pro-Mihail.local>
2023-05-31 23:00:33 +0500

    Добавлены изменения в код

--- main.go
+++ main.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/labstack/echo/v4"
-	"net"
-	"net/http"
-	"os"
-	"sync"
-	"time"
+	"encoding/json"               // Пакет для кодирования и декодирования JSON
+	"fmt"                         // Пакет для форматированного ввода-вывода
+	"github.com/labstack/echo/v4" // Внешний пакет для создания веб-приложений
+	"net"                         // Пакет для операций сети
+	"net/http"                    // Пакет для функциональности HTTP-клиента и HTTP-сервера
+	"os"                          // Пакет предоставляет возможность использования функций, зависящих от операционной системы
+	"sync"                        // Пакет для предоставления базовых примитивов синхронизации, таких как мьютексы
+	"time"                        // Пакет для работы со временем
 )
 
 type Server struct {
-	IP string `json:"ip"`
+	IP string `json:"ip"` // Структура, представляющая сервер с IP-адресом
 }
 
 type PingResult struct {
-	IP     string `json:"ip"`
-	Status string `json:"status"`
+	IP     string `json:"ip"`     // Структура, представляющая IP-адрес пингуемого сервера
+	Status string `json:"status"` // Структура, представляющая статус пинга (например, Ok или Error)
 }
 
 type PingResults struct {
 	sync.Mutex
-	Results map[string]string `json:"results"`
+	Results map[string]string `json:"results"` // Структура, представляющая результаты пингования нескольких серверов
 }
 
 func main() {
-	pingServers := func(servers []Server) *PingResults {
+	pingServers := func(servers []Server) *PingResults { // Функция для пингования нескольких серверов и сбора результатов
 		var wg sync.WaitGroup
 		results := &PingResults{
 			Results: make(map[string]string),
@@ -36,7 +36,7 @@ func main() {
 			wg.Add(1)
 			go func(ip string) {
 				status := "Ok"
-				conn, err := net.DialTimeout("tcp", ip+":80", 2*time.Second)
+				conn, err := net.DialTimeout("tcp", ip+":80", 2*time.Second) // Установка TCP-соединения с сервером
 				if err != nil {
 					status = "Error"
 				} else {
@@ -44,7 +44,7 @@ func main() {
 				}
 
 				results.Lock()
-				results.Results[ip] = status
+				results.Results[ip] = status // Сохранение статуса пинга сервера
 				results.Unlock()
 				wg.Done()
 			}(s.IP)
@@ -54,56 +54,56 @@ func main() {
 		return results
 	}
 
-	serversFile, err := ReadFile("servers.json")
+	serversFile, err := ReadFile("servers.json") // Чтение файла конфигурации серверов
 	if err != nil {
 		panic(err)
 	}
 
 	var servers []Server
-	if err := json.Unmarshal(serversFile, &servers); err != nil {
+	if err := json.Unmarshal(serversFile, &servers); err != nil { // Распаковка JSON-данных в структуру серверов
 		panic(err)
 	}
 
-	e := echo.New()
+	e := echo.New() // Создание нового экземпляра веб-фреймворка Echo
 
-	e.GET("/ping", func(c echo.Context) error {
-		results := pingServers(servers)
+	e.GET("/ping", func(c echo.Context) error { // Обработка GET-запроса к "/ping"
+		results := pingServers(servers) // Пингование серверов и получение результатов
 
-		fmt.Println("Ping Results:")
-		for ip, status := range results.Results {
-			fmt.Printf("Server: %s, Status: %s\n", ip, status)
+		fmt.Println("Результаты пинга:")
+		for ip, status := range results.Results { // Вывод результатов пинга
+			fmt.Printf("Сервер: %s, Статус: %s\n", ip, status)
 		}
 
-		go func() {
+		go func() { // Запуск горутины для периодического пингования серверов
 			for range time.Tick(10 * time.Second) {
 				results := pingServers(servers)
-				fmt.Println("Ping Results:")
+				fmt.Println("Результаты пинга:")
 				for ip, status := range results.Results {
-					fmt.Printf("Server: %s, Status: %s\n", ip, status)
+					fmt.Printf("Сервер: %s, Статус: %s\n", ip, status)
 				}
 			}
 		}()
 
-		return c.JSON(http.StatusOK, results)
+		return c.JSON(http.StatusOK, results) // Возврат результатов пинга в виде JSON-ответа
 	})
 
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(":8080")) // Запуск сервера и прослушивание порта 8080
 }
 
-func ReadFile(path string) ([]byte, error) {
-	file, err := os.Open(path)
+func ReadFile(path string) ([]byte, error) { // Функция для чтения содержимого файла
+	file, err := os.Open(path) // Открытие файла
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 
-	stat, err := file.Stat()
+	stat, err := file.Stat() // Получение информации о файле
 	if err != nil {
 		return nil, err
 	}
 
 	buf := make([]byte, stat.Size())
-	_, err = file.Read(buf)
+	_, err = file.Read(buf) // Чтение данных из файла
 	if err != nil {
 		return nil, err
 	}
